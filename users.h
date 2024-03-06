6a5cb86e0ae65e960743384a87d804fdaa21d1f3
Mariam <YourEmailgit>
2024-03-06 10:26:30 +0200

    users header

---
+++ users.h
@@ -0,0 +1,16 @@
+#ifndef USERS_H
+#define USERS_H
+
+#include<QString>
+extern QString usernames[100];
+extern QString passwords[100];
+extern int ages[100];
+extern int usersCount;
+
+class Users
+{
+public:
+    Users();
+};
+
+#endif // USERS_H
